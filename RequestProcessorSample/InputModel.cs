5ff237202ed96cae1e65bd35e9d2fe49092089c4
Valentas Klimas <valentasklimas@gmail.com>
2020-06-11 10:50:08 +0100

    Input Model added

--- RequestProcessorSample/InputModel.cs
+++ RequestProcessorSample/InputModel.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace RequestProcessorSample
 {
-   public class InputModel // holds various data which will be sent through the functions
+   public class InputModel // holds various data which will be sent through the functions 
     {
         public string Input { get; set; }
         public string Response { get; set; }
