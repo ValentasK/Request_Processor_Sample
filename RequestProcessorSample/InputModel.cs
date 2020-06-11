7e90aa4b1eb14984fa8cf2bce220cc07fdf5d3e8
Valentas Klimas <valentasklimas@gmail.com>
2020-06-11 10:49:38 +0100

    Add project files.

---
+++ RequestProcessorSample/InputModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RequestProcessorSample
+{
+   public class InputModel // holds various data which will be sent through the functions
+    {
+        public string Input { get; set; }
+        public string Response { get; set; }
+    }
+}
---
+++ RequestProcessorSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RequestProcessorSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
