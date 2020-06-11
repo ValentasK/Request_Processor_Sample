002f625229608199a537843656d91136a9ec556d
Valentas Klimas <valentasklimas@gmail.com>
2020-06-11 10:52:52 +0100

    Input Processor class added

---
+++ RequestProcessorSample/InputProcessor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RequestProcessorSample
+{
+    public class InputProcessor
+    {
+        public delegate void ProcessorDelegate(InputModel inputModel);
+        public List<ProcessorDelegate> Processors { get; set; } = new List<ProcessorDelegate>();
+        public bool Processing { get; set; }
+
+        public void Process()
+        {
+            while (true)
+            {
+                Processing = true;
+                Console.WriteLine("Waiting for input");
+                var a = Console.ReadLine();
+                InputModel inputModel = new InputModel()
+                {
+                    Input = a,
+                    Response = "OK"
+                };
+
+                // processing happens here 
+
+                foreach (ProcessorDelegate processor in Processors)
+                {
+                    if (this.Processing)
+                    {
+                        processor(inputModel);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                //
+                if (Processing)
+                {
+                    WriteResponse(inputModel);
+                }
+            }
+        }
+
+        public void WriteResponse(InputModel inputModel)
+        {
+            this.Processing = false;
+            Console.WriteLine(inputModel.Response);
+        }
+    }
+}
